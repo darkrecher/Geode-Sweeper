6b3cbf932af7e129cec1ec6cd19cdd9bd1c9e2c3
Réchèr <recher_burp@yahoo.fr>
2014-12-25 20:17:58 +0100

    création mesh icosaèdre et rainbow-cube

--- main.py
+++ main.py
@@ -41,43 +41,63 @@ def group2(iterator, count):
     """
     return itertools.imap(None, *([ iter(iterator) ] * count))
 
+def build_mesh_rainbow_cube():
 
-cube_list_vertices = (
-    1, 1, 1, #0
-    -1, 1, 1, #1
-    -1, -1, 1, #2
-    1, -1, 1, #3
-    1, 1, -1, #4
-    -1, 1, -1, #5
-    -1, -1, -1, #6
-    1, -1, -1 #7
-)
+    coords_cube = (
+        (1, 1, 1), 
+        (-1, 1, 1), 
+        (-1, -1, 1), 
+        (1, -1, 1), 
+        (1, 1, -1), 
+        (-1, 1, -1), 
+        (-1, -1, -1), 
+        (1, -1, -1), 
+    )
 
-cube_list_colors = (
-    1.0, 1.0, 1.0,
-    1.0, 1.0, 0.2,
-    1.0, 0.2, 0.2,
-    1.0, 0.2, 1.0,
-    0.2, 1.0, 1.0,
-    0.2, 1.0, 0.2,
-    0.2, 0.2, 0.2,
-    0.2, 0.2, 1.0,
-)
+    colors_cube = (
+        (1.0, 1.0, 1.0),
+        (1.0, 1.0, 0.2),
+        (1.0, 0.2, 0.2),
+        (1.0, 0.2, 1.0),
+        (0.2, 1.0, 1.0),
+        (0.2, 1.0, 0.2),
+        (0.2, 0.2, 0.2),
+        (0.2, 0.2, 1.0),
+    )
 
-cube_list_index_for_planes = (
-    0, 1, 2, 3, # front face
-    0, 4, 5, 1, # top face
-    4, 0, 3, 7, # right face
-    1, 5, 6, 2, # left face
-    3, 2, 6, 7, # bottom face
-    4, 7, 6, 5, # back face
-)
+    indexes_vertex_cube = (
+        0, 1, 2, 3, # front face
+        0, 4, 5, 1, # top face
+        4, 0, 3, 7, # right face
+        1, 5, 6, 2, # left face
+        3, 2, 6, 7, # bottom face
+        4, 7, 6, 5, # back face
+    )
+
+    indexes_vertex_cube_grouped_by_plane = group2(indexes_vertex_cube, 4)
+    grouping_plane_to_triangles = (0, 1, 2, 0, 2, 3)    
+    mesh_vertices = []
+    mesh_colors = []
+    for indexes_one_plane in indexes_vertex_cube_grouped_by_plane:
+        indexes_grouped_by_triangles = (
+            indexes_one_plane[grouping_modification]
+            for grouping_modification
+            in grouping_plane_to_triangles)
+        for one_index in indexes_grouped_by_triangles:
+            coords_vertex = coords_cube[one_index]
+            mesh_vertices.extend(coords_vertex)
+            color = colors_cube[one_index]
+            mesh_colors.extend(color)
+
+    mesh_glfloat_vertices = vector(pgl.GLfloat, *mesh_vertices)
+    mesh_glfloat_colors = vector(pgl.GLfloat, *mesh_colors)
+    return mesh_glfloat_vertices, mesh_glfloat_colors
+    
 
 def build_mesh_icosahedron():
     
     h_prime_sqr = math.sin(math.pi / 10.0) * math.sin((3.0 * math.pi) / 10.0)
     h_prime = math.sqrt(h_prime_sqr)
-
     h_second_sqr = 1.0 - (0.25 / (math.sin(math.pi/5)) ** 2)
     h_second = math.sqrt(h_second_sqr)
 
@@ -105,48 +125,42 @@ def build_mesh_icosahedron():
 
     indexes_vertex_and_colors_icosahedron = (
         # couronne du haut
-        (0, 1, 2, 1.0, 1.0, 1.0, ),
-        (0, 2, 3, 0.9, 0.9, 0.9, ),
-        (0, 3, 4, 0.8, 0.8, 0.8, ),
-        (0, 4, 5, 0.7, 0.7, 0.7, ),
-        (0, 5, 1, 0.9, 0.9, 0.9, ),
+        ((0, 1, 2),   (1.0, 1.0, 1.0, )),
+        ((0, 2, 3),   (0.9, 0.9, 0.9, )),
+        ((0, 3, 4),   (0.8, 0.8, 0.8, )),
+        ((0, 4, 5),   (0.7, 0.7, 0.7, )),
+        ((0, 5, 1),   (0.9, 0.9, 0.9, )),
         # bande du milieu (triangle groupe 1)
-        (1, 2, 6,  0.7, 1.0, 0.7, ),
-        (2, 3, 7,  0.6, 1.0, 0.6, ),
-        (3, 4, 8,  0.5, 1.0, 0.5, ),
-        (4, 5, 9,  0.4, 1.0, 0.4, ),
-        (5, 1, 10, 0.6, 1.0, 0.6, ),
+        ((1, 2, 6),   (0.7, 1.0, 0.7, )),
+        ((2, 3, 7),   (0.6, 1.0, 0.6, )),
+        ((3, 4, 8),   (0.5, 1.0, 0.5, )),
+        ((4, 5, 9),   (0.4, 1.0, 0.4, )),
+        ((5, 1, 10),  (0.6, 1.0, 0.6, )),
         # bande du milieu (triangle groupe 2)
-        (6, 7, 2,  0.3, 1.0, 0.3, ),
-        (7, 8, 3,  0.2, 1.0, 0.2, ),
-        (8, 9, 4,  0.1, 1.0, 0.1, ),
-        (9, 10, 5, 0.0, 1.0, 0.0, ),
-        (10, 6, 1, 0.2, 1.0, 0.2, ),
+        ((6, 7, 2),   (0.3, 1.0, 0.3, )),
+        ((7, 8, 3),   (0.2, 1.0, 0.2, )),
+        ((8, 9, 4),   (0.1, 1.0, 0.1, )),
+        ((9, 10, 5),  (0.0, 1.0, 0.0, )),
+        ((10, 6, 1),  (0.2, 1.0, 0.2, )),
         # couronne du bas
-        (11, 6, 7,  0.0, 0.9, 0.0, ),
-        (11, 7, 8,  0.0, 0.8, 0.0, ),
-        (11, 8, 9,  0.0, 0.7, 0.0, ),
-        (11, 9, 10, 0.0, 0.6, 0.0, ),
-        (11, 10, 6, 0.0, 0.8, 0.0, ),
+        ((11, 6, 7),  (0.0, 0.9, 0.0, )),
+        ((11, 7, 8),  (0.0, 0.8, 0.0, )),
+        ((11, 8, 9),  (0.0, 0.7, 0.0, )),
+        ((11, 9, 10), (0.0, 0.6, 0.0, )),
+        ((11, 10, 6), (0.0, 0.8, 0.0, )),
     )
 
     mesh_vertices = []
     mesh_colors = []
-
-    for elem in indexes_vertex_and_colors_icosahedron:
-        indexes = elem[0:3]
-        color = elem[3:]
-        for vertex_index in indexes:
-            vertex = coords_icosahedron[vertex_index]
-            for coord in vertex:
-                mesh_vertices.append(coord)
+    for indexes, color in indexes_vertex_and_colors_icosahedron:
+        for one_index in indexes:
+            coords_one_vertex = coords_icosahedron[one_index]
+            mesh_vertices.extend(coords_one_vertex)
         for _ in range(3):
-            for color_coord in color:
-                mesh_colors.append(color_coord)
+            mesh_colors.extend(color)
 
     mesh_glfloat_vertices = vector(pgl.GLfloat, *mesh_vertices)
     mesh_glfloat_colors = vector(pgl.GLfloat, *mesh_colors)
-
     return mesh_glfloat_vertices, mesh_glfloat_colors
 
 
@@ -232,7 +246,8 @@ windowGeodeSweeper = WindowGeodeSweeper(
     fullscreen=False, vsync=False, resizable=True,
     height=600, width=600)
 
-mesh_glfloat_vertices, mesh_glfloat_colors = build_mesh_icosahedron()
+mesh_glfloat_vertices, mesh_glfloat_colors = build_mesh_rainbow_cube()
+#mesh_glfloat_vertices, mesh_glfloat_colors = build_mesh_icosahedron()
 windowGeodeSweeper.init_geode_sweeper(
     mesh_glfloat_vertices,
     mesh_glfloat_colors)
