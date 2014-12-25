d4c00e110ac416e4be0f13d00cbf1726efa09f20
Réchèr <recher_burp@yahoo.fr>
2014-12-25 23:27:42 +0100

    Deux main.py : cube et icosahedron.

---
+++ main_icosahedron.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import (unicode_literals, absolute_import,
+                        print_function, division)
+
+import pyglet
+pgl = pyglet.gl
+
+import window_geode_sweeper
+win = window_geode_sweeper.window_geode_sweeper
+PERIOD_GAME_CYCLE_SECONDS = window_geode_sweeper.PERIOD_GAME_CYCLE_SECONDS
+from mesh_builder import MeshRainbowCube, MeshIcosahedron
+
+mesh = MeshIcosahedron()
+win.init_geode_sweeper(mesh)
+
+pyglet.clock.schedule_interval(win.update, PERIOD_GAME_CYCLE_SECONDS)
+pyglet.app.run()
