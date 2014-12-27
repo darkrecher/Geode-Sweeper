c370fac11b11714d2f6218659cd6e3e8fafd135a
Réchèr <recher_burp@yahoo.fr>
2014-12-27 17:30:44 +0100

    docstrings bat_belt.

--- bat_belt.py
+++ bat_belt.py
@@ -1,20 +1,48 @@
 # -*- coding: utf-8 -*-
 
+"""
+    Module contenant quelques fonctions de base.
+"""
+
 from __future__ import (unicode_literals, absolute_import,
                         print_function, division)
-
 import itertools
+
 
 def ctype_array(gl_type, *args):
     """
-        renvoie un tableau à une dimension contenant des variables ctype.
-        gl_type : type des données du tableau : GLfloat, GLuint, ...
-        args : donnée du tableau à convertir en ctype.
+    Fonction utile avec la libraire pyglet
+    (mais pourrait servir à d'autres occasions).
+    Renvoie un tableau à une dimension contenant des variables ctype.
+
+    :Example:
+    >>> import pyglet
+    >>> ctype_array_floats = ctype_array(pyglet.gl.GLfloat, 0.5, 1.0, 1.5)
+    >>> ctype_array_floats
+    <bat_belt.c_float_Array_3 object at 0x01DDDF30>
+    >>> ctype_array_floats[0]
+    0.5
+    
+    :param gl_type: type des données du tableau. GLfloat, GLuint, ...
+    :param *args: valeurs float. Données à mettre dans le tableau de ctype.
     """
     return (gl_type*len(args))(*args)
 
 def group2(iterator, count):
     """
-    Prend ce qui sort de iterator, et en fait des paquets de "count" elements.
+    Regroupe par paquet.
+    Construit des tuples de "count" elements,
+    à partir de ce qui sort de iterator.
+    Ne renvoit pas la fin de l'itérateur
+    si ça ne tombe pas juste par rapport à "count"
+
+    :Example:
+    >>> iter_group2 = bat_belt.group2(range(5, 15), 4)
+    >>> iter_group2
+    <itertools.imap object at 0x018D85F0>
+    >>> list(iter_group2)
+    [(5, 6, 7, 8), (9, 10, 11, 12)]
+    # On ne récupère pas 13 et 14.
     """
     return itertools.imap(None, *([ iter(iterator) ] * count))
+
