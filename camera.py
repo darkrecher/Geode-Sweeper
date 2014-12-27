77271f04eb82eba9adc7a7d64269f6d4d1447212
Réchèr <recher_burp@yahoo.fr>
2014-12-27 18:21:56 +0100

    amélioration classe camera.py
    nom plus explicite : CameraAroundSphere
    La position et le point de vue sont stockée dans des Vector3D.
    Fonction pour se déplacer dans la sphère, en utilisant les variables membres internes delta_lateral et delta_longitudinal.

--- camera.py
+++ camera.py
@@ -9,58 +9,55 @@ from __future__ import (unicode_literals, absolute_import,
 from vector3D import Vector3D
 
 
-class Camera(object):
+class CameraAroundSphere(object):
 
     def __init__(self):
-
-        self.delta_x = 0.0
-        self.delta_y = 0.0
-
-        self.x = 0.0
-        self.y = 0.0
-        self.z = -1.0
-
+        self.pos = Vector3D(0.0, 0.0, -1.0)
+        self.lookat = Vector3D(0.0, 0.0, 0.0)
         self.v_front = Vector3D(0.0, 0.0, 1.0)
         self.v_left = Vector3D(1.0, 0.0, 0.0)
         self.v_up = self.v_left.cross_product(self.v_front)
+        self.delta_lateral = 0.0
+        self.delta_longitudinal = 0.0        
 
     def _refresh_vfront(self):
-        self.v_front = Vector3D(-self.x, -self.y, -self.z)
+        # TODO : addition/multiplication de Vector3D
+        self.v_front = Vector3D(-self.pos.x, -self.pos.y, -self.pos.z)
         self.v_front.normify()
 
     def _get_back_to_sphere(self):
-        # TODO : c'est pas plus simple de définir directement les xyz à -v_front ?
-        v_pos_to_center = Vector3D(-self.x, -self.y, -self.z)
-        v_pos_to_sphere = Vector3D(
-            v_pos_to_center.x - self.v_front.x,
-            v_pos_to_center.y - self.v_front.y,
-            v_pos_to_center.z - self.v_front.z)
-        self.x += v_pos_to_sphere.x
-        self.y += v_pos_to_sphere.y
-        self.z += v_pos_to_sphere.z
+        # TODO : addition/multiplication de Vector3D
+        self.pos = Vector3D(-self.v_front.x, -self.v_front.y, -self.v_front.z)
 
-    def slide_lateral(self, dist):
-        self.x += self.v_left.x * dist
-        self.y += self.v_left.y * dist
-        self.z += self.v_left.z * dist
+    def _slide_lateral(self, dist):
+        # TODO : addition/multiplication de Vector3D
+        self.pos.x += self.v_left.x * dist
+        self.pos.y += self.v_left.y * dist
+        self.pos.z += self.v_left.z * dist
         self._refresh_vfront()
         self._get_back_to_sphere()
         self.v_left = self.v_front.cross_product(self.v_up)
 
-    def slide_longitudinal(self, dist):
-        self.x += self.v_up.x * dist
-        self.y += self.v_up.y * dist
-        self.z += self.v_up.z * dist
+    def _slide_longitudinal(self, dist):
+        # TODO : addition/multiplication de Vector3D
+        self.pos.x += self.v_up.x * dist
+        self.pos.y += self.v_up.y * dist
+        self.pos.z += self.v_up.z * dist
         self._refresh_vfront()
         self._get_back_to_sphere()
         self.v_up = self.v_left.cross_product(self.v_front)
 
+    def slide_with_deltas(self):
+        if self.delta_lateral != 0.0:
+            self._slide_lateral(self.delta_lateral)
+        if self.delta_longitudinal != 0.0:
+            self._slide_longitudinal(self.delta_longitudinal)
+        
 
 if __name__ == "__main__":
     
     # tests unitaires.
-    cam = Camera()
+    cam = CameraAroundSphere()
     print("up : ", cam.v_up)
     print("left : ", cam.v_front.cross_product(cam.v_up))
 
-        
