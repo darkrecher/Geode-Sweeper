659ee0d4ef8dbdb68bd21ae11852f448ba1b76fe
Réchèr <recher_burp@yahoo.fr>
2014-12-27 17:31:35 +0100

    docstrings Vector3D

--- vector3D.py
+++ vector3D.py
@@ -1,66 +1,77 @@
 # -*- coding: utf-8 -*-
 
-"""
-Définit un vecteur dans un espace en 3 dimensions.
-Avec quelques fonctions de base : normage, produit en croix.
-Les fonctions de base-base (addition, multiplication) ne sont pas présentes.
-Vous avez été prévenu.
-"""
-
 from __future__ import (unicode_literals, absolute_import,
                         print_function, division)
-
 import math
 
-                        
+
 class Vector3D(object):
+
+    """
+    Définit un vecteur dans un espace en 3 dimensions.
+    Avec quelques fonctions de base : normage, produit en croix.
+    Les fonctions de base-base (add, multiply) ne sont pas présentes.
+    Vous avez été prévenu.
+    """
 
     # TODO : trouver une putain de classe vecteur toute faite.
     # Je vais pas me faire chier à tout recoder.
 
     def __init__(self, x, y, z):
-        # TODO : convertir en float.
         self.x = x
         self.y = y
         self.z = z
 
     def __str__(self):
+        """
+        Représentation du Vector3D.
+        Écrit les valeurs des 3 coordonnées x, y, z.
+        """
         return "".join((
-            "vect3D:",
+            "<vector3D:",
             " x:", str(self.x),
             " y:", str(self.y),
             " z:", str(self.z),
+            ">",
         ))
 
     def normify(self):
-        current_norm = math.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+        """
+        Modifie les coordonnées x, y, z, de façon à ce que la norme (longueur)
+        du Vector3D soit égale à 1.
+        Le sens et la direction du Vector3D ne sont pas modifiées.
+        """
+        current_norm = math.sqrt(sum((self.x ** 2, self.y ** 2, self.z ** 2)))
         self.x /= current_norm
         self.y /= current_norm
         self.z /= current_norm
 
     def cross_product(self, v_other):
-
-        u1 = self.x
-        u2 = self.y
-        u3 = self.z
-        v1 = v_other.x
-        v2 = v_other.y
-        v3 = v_other.z
+        """
+        Renvoit un nouveau Vector3D, correspondant au produit vectoriel
+        (produit en croix) de ce Vector3D, et d'un second Vector3D (v_other).
+        Pour une description de la méthode de calcul du produit en croix :
+        http://en.wikipedia.org/wiki/Cross_product
+        """
+        u1, u2, u3 = self.x, self.y, self.z
+        v1, v2, v3 = v_other.x, v_other.y, v_other.z
 
         prod_x = u2*v3 - u3*v2
         prod_y = u3*v1 - u1*v3
         prod_z = u1*v2 - u2*v1
 
-        # http://en.wikipedia.org/wiki/Cross_product
         return Vector3D(prod_x, prod_y, prod_z)
 
+
+def main():
+    """
+    Tests unitaires (pas exhaustif du tout).
+    """
+    test_normify = Vector3D(1.0, 2.0, 3.0)
+    print("test_normify init : ", test_normify)
+    test_normify.normify()
+    print("test_normify normé : ", test_normify)
         
 if __name__ == "__main__":
-    
-    # tests unitaires.
-    test_normify = Vector3D(1.0, 2.0, 3.0)
-    print("test_normify: ", test_normify)
-    test_normify.normify()
-    print("test_normify normé: ", test_normify)
+    main()
 
-    
