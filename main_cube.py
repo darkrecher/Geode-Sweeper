80ebd223f6ac47fab33e13ea706aa8a4b913b6ea
Réchèr <recher_burp@yahoo.fr>
2014-12-28 22:47:02 +0100

    docstring main_cube.py

--- main_cube.py
+++ main_cube.py
@@ -1,19 +1,29 @@
 # -*- coding: utf-8 -*-
 
+"""
+Initialisation et lancement d'une application pyglet WindowGeodeSweeper.
+Affiche un cube multicolore "arc-en-ciel".
+"""
+
 from __future__ import (unicode_literals, absolute_import,
                         print_function, division)
-
 import pyglet
 pgl = pyglet.gl
 
 import window_geode_sweeper
 win = window_geode_sweeper.window_geode_sweeper
-PERIOD_GAME_CYCLE_SECONDS = window_geode_sweeper.PERIOD_GAME_CYCLE_SECONDS
 from mesh_builder import MeshRainbowCube, MeshIcosahedron
 
-mesh = MeshRainbowCube()
-win.init_geode_sweeper(mesh)
 
-pyglet.clock.schedule_interval(win.update, PERIOD_GAME_CYCLE_SECONDS)
-pyglet.app.run()
+def main():
+    mesh = MeshRainbowCube()
+    win.init_geode_sweeper(mesh)
+
+    pyglet.clock.schedule_interval(
+        win.update,
+        window_geode_sweeper.PERIOD_GAME_CYCLE_SECONDS)
+    pyglet.app.run()
+
+if __name__ == "__main__":
+    main()
 
