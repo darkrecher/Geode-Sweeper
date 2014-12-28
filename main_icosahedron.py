65df1ecb38b63c01d3a26237aa10fd948e8b86cf
Réchèr <recher_burp@yahoo.fr>
2014-12-28 22:46:16 +0100

    docstring main_icosahedron.py

--- main_icosahedron.py
+++ main_icosahedron.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+"""
+Initialisation et lancement d'une application pyglet WindowGeodeSweeper.
+Affiche un icosaèdre blanc et vert.
+"""
+
 from __future__ import (unicode_literals, absolute_import,
                         print_function, division)
 
@@ -8,11 +13,18 @@ pgl = pyglet.gl
 
 import window_geode_sweeper
 win = window_geode_sweeper.window_geode_sweeper
-PERIOD_GAME_CYCLE_SECONDS = window_geode_sweeper.PERIOD_GAME_CYCLE_SECONDS
 from mesh_builder import MeshRainbowCube, MeshIcosahedron
 
-mesh = MeshIcosahedron()
-win.init_geode_sweeper(mesh)
 
-pyglet.clock.schedule_interval(win.update, PERIOD_GAME_CYCLE_SECONDS)
-pyglet.app.run()
+def main():
+    mesh = MeshIcosahedron()
+    win.init_geode_sweeper(mesh)
+
+    pyglet.clock.schedule_interval(
+        win.update,
+        window_geode_sweeper.PERIOD_GAME_CYCLE_SECONDS)
+    pyglet.app.run()
+
+if __name__ == "__main__":
+    main()
+
