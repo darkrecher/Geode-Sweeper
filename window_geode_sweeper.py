954ac535ef52c1dc229dd64ffea8c1dd62383584
Réchèr <recher_burp@yahoo.fr>
2014-12-28 10:09:59 +0100

    amélioration code window_geode_sweeper.py
    Utilisation de constantes.
    Utilisation d'un dictionnaire pour associer les touches aux fonctions, plutôt qu'un tas de if.
    Le dessin de l'objet 3D n'est plus déformé par la taille de la fenêtre de l'application.

--- window_geode_sweeper.py
+++ window_geode_sweeper.py
@@ -26,17 +26,40 @@ PERIOD_GAME_CYCLE_SECONDS = 0.01
 
 class WindowGeodeSweeper(pyglet.window.Window):
 
+    # Diverses constantes, dont les valeurs ont été déterminées au feeling.
+
+    # paramètres fovy, zNear, zFar.
+    # Voir documentation de la fonction gluPerspective
+    # https://www.opengl.org/sdk/docs/man2/xhtml/gluPerspective.xml
+    FIELD_OF_VIEW_ANGLE_Y = 40.0
+    Z_NEAR = 1.0
+    Z_FAR = 40.0
+
+    # Distance entre la caméra et le centre de l'espace O.
+    # (La caméra se déplace sur une sphère centrée en O, de rayon DIST_CAM).
+    DIST_CAM = 15
+
+    # Facteur d'agrandissement des coordonnées de l'espace.
+    SCALE_COORD = 3.0
+
+    # Distance de déplacement orthogonal de la caméra, à chaque cycle.
+    CAM_DELTA = 0.05
+
     def init_geode_sweeper(self, mesh):
         self.mesh = mesh
-        self.cam = CameraAroundSphere()
-        pgl.glEnable(pgl.GL_DEPTH_TEST)
+        self.cam = CameraAroundSphere()        
+        pgl.glEnable(pgl.GL_DEPTH_TEST)        
 
     def on_resize(self, width, height):
         pgl.glViewport(0, 0, width, height)
         pgl.glMatrixMode(pgl.GL_PROJECTION)
         pgl.glLoadIdentity()
-        # TODO : width/height
-        pgl.gluPerspective(40.0, 1.0, 1.0, 40.0)
+        if height != 0:
+            pgl.gluPerspective(
+                WindowGeodeSweeper.FIELD_OF_VIEW_ANGLE_Y,
+                width / height,
+                WindowGeodeSweeper.Z_NEAR,
+                WindowGeodeSweeper.Z_FAR)
         return pyglet.event.EVENT_HANDLED
 
     def on_draw(self):
@@ -46,20 +69,19 @@ class WindowGeodeSweeper(pyglet.window.Window):
 
         pgl.glMatrixMode(pgl.GL_MODELVIEW)
         pgl.glLoadIdentity()
+        pos_final = self.cam.pos * WindowGeodeSweeper.DIST_CAM
         pgl.gluLookAt(
-            self.cam.pos.x * 15, self.cam.pos.y * 15, self.cam.pos.z * 15,
+            pos_final.x, pos_final.y, pos_final.z, 
             self.cam.lookat.x, self.cam.lookat.y, self.cam.lookat.z,
             self.cam.v_up.x, self.cam.v_up.y, self.cam.v_up.z)
-        pgl.glScalef(3.0, 3.0, 3.0)
+        scale_coord = WindowGeodeSweeper.SCALE_COORD
+        pgl.glScalef(scale_coord, scale_coord, scale_coord)
 
         pgl.glEnableClientState(pgl.GL_VERTEX_ARRAY)
         pgl.glEnableClientState(pgl.GL_COLOR_ARRAY)
 
         pgl.glColorPointer(3, pgl.GL_FLOAT, 0, self.mesh.glfloat_colors)
         pgl.glVertexPointer(3, pgl.GL_FLOAT, 0, self.mesh.glfloat_vertices)
-        # pgl.glDrawElements(
-        #     pgl.GL_TRIANGLES, len(solid_index_for_planes),
-        #     pgl.GL_UNSIGNED_INT, solid_index_for_planes)
 
         # Attention au dernier param !
         # On n'indique pas le nombre d'élément du tableau,
@@ -73,24 +95,42 @@ class WindowGeodeSweeper(pyglet.window.Window):
         pgl.glDisableClientState(pgl.GL_COLOR_ARRAY)
         pgl.glDisableClientState(pgl.GL_VERTEX_ARRAY)
 
+    def move_lat_right(self):
+        self.cam.delta_lateral = WindowGeodeSweeper.CAM_DELTA
+    def move_lat_left(self):
+        self.cam.delta_lateral = -WindowGeodeSweeper.CAM_DELTA
+    def move_longi_up(self):
+        self.cam.delta_longitudinal = -WindowGeodeSweeper.CAM_DELTA
+    def move_longi_down(self):
+        self.cam.delta_longitudinal = WindowGeodeSweeper.CAM_DELTA
+
     def on_key_press(self, symbol, modifiers):
-        # TODO : dict touche -> fonction.
-        if symbol == pyglet.window.key.RIGHT:
-            self.cam.delta_lateral = 0.05
-        elif symbol == pyglet.window.key.LEFT:
-            self.cam.delta_lateral = -0.05
-        if symbol == pyglet.window.key.UP:
-            self.cam.delta_longitudinal = -0.05
-        elif symbol == pyglet.window.key.DOWN:
-            self.cam.delta_longitudinal = 0.05
-        elif symbol == pyglet.window.key.ESCAPE:
-            self.close()
+        function_from_key_pressed = {
+            pyglet.window.key.RIGHT : self.move_lat_right,
+            pyglet.window.key.LEFT : self.move_lat_left,
+            pyglet.window.key.UP : self.move_longi_up,
+            pyglet.window.key.DOWN : self.move_longi_down,
+            pyglet.window.key.ESCAPE : self.close,
+        }
+        function_to_exec = function_from_key_pressed.get(symbol)
+        if function_to_exec is not None:
+            function_to_exec()
+
+    def stop_move_lat(self):
+        self.cam.delta_lateral = 0.0
+    def stop_move_longi(self):
+        self.cam.delta_longitudinal = 0.0
 
     def on_key_release(self, symbol, modifiers):
-        if symbol in (pyglet.window.key.RIGHT, pyglet.window.key.LEFT):
-            self.cam.delta_lateral = 0.0
-        if symbol in (pyglet.window.key.UP, pyglet.window.key.DOWN):
-            self.cam.delta_longitudinal = 0.0
+        function_from_key_released = {
+            pyglet.window.key.RIGHT : self.stop_move_lat,
+            pyglet.window.key.LEFT : self.stop_move_lat,
+            pyglet.window.key.UP : self.stop_move_longi,
+            pyglet.window.key.DOWN : self.stop_move_longi,
+        }
+        function_to_exec = function_from_key_released.get(symbol)
+        if function_to_exec is not None:
+            function_to_exec()
 
     def update(self, dt):
         self.cam.slide_with_deltas()
